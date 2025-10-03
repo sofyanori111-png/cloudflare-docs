fc02c966e888262e82fe94d85223171ed40b8d4d
Kody Jackson <kody@cloudflare.com>
2025-10-03 12:41:44 -0500

    Update bin/validate-redirects.ts

--- bin/validate-redirects.ts
+++ bin/validate-redirects.ts
@@ -7,6 +7,8 @@ async function main() {
 	let numUrlsWithFragment = 0;
 	let numDuplicateRedirects = 0;
 	let numNonSlashedRedirects = 0;
+	
+	const validEndings = ["/", "*", ".xml", ".md", ".json", ".html"];
 
 	const redirectSourceUrls: string[] = [];
 
